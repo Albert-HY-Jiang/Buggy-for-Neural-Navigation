aa6bef5562a29bdaa52070b33cf1fa208715e42b
Albert-HY-Jiang <112735990+Albert-HY-Jiang@users.noreply.github.com>
2025-06-18 21:17:01 +0800

    Delete src/motor.h

--- src/motor.h
+++
@@ -1,9 +0,0 @@
-#include "msp.h"
-#include <stdio.h>
-#include "SysTick0.h"
-
-void motors_init(); // Initialize ports controlling H-Bridge
-
-void forward(int time); // Move buggy forward for a time in ms
-void backward(int time); // Move buggy backward for a time in ms
-void turn(int angle); // Turn buggy by an angle in degrees
